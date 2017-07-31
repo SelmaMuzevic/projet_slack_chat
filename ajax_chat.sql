9b7039a7542518d2792a074ab4d06c736e87ae29
selma <selma@gmail.com>
2017-07-31 17:26:57 +0200

    creation de database et la table message

--- ajax_chat.sql
+++ ajax_chat.sql
@@ -1,14 +1,14 @@
-DROP DATABASE IF EXISTS 'ajax_chat';
+DROP DATABASE IF EXISTS `ajax_chat`;
 
-CREATE DATABASE 'ajax_chat' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `ajax_chat` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 DROP USER 'ajax_chat_user'@'localhost';
 
 CREATE USER 'ajax_chat_user'@'localhost' IDENTIFIED BY 'We love SQL API !';
 
-GRANT ALL PRIVILEGES ON 'ajax_chat' .* TO 'ajax_chat_user'@'localhost';
+GRANT ALL PRIVILEGES ON `ajax_chat` .* TO 'ajax_chat_user'@'localhost';
 
-USE 'ajax_chat';
+USE `ajax_chat`;
 
 CREATE TABLE 'message' (
     'Id' INT AUTO_INCREMENT PRIMARY KEY, 
